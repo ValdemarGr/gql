7888612a28f258cd7629bfceffa4af8b4a098921
Valdemar Grange <randomvald0069@gmail.com>
2022-07-09 20:14:58 +0200

    wok

--- src/main/scala/gql/Optimizer.scala
+++ src/main/scala/gql/Optimizer.scala
@@ -4,6 +4,9 @@ package gql
  * The executable GraphQL query is a tree of potentially effectful functions to run.
  * The optimizers job is to pick the most efficient way to evaluate this tree.
  * Queries are batched by running all nodes on the same level of the tree (potentially) together.
+ * Let A be a node with an arbitary number of children: |children(A)| \geq 0
+ * Let Defer be a special node that lets the interpreter defer evalutaion until the next level.
+ * Let [A] be a contracted set of nodes that can be effeciently evaluated together.
  * As an example consider rewriting the following query:
  *            A____
  *           / \   \
@@ -16,7 +19,41 @@ package gql
  *          B Defer
  *        / \  / \
  *       D  D D   D
- * Now we can run nodes D together.
+ *
+ * A naive implementation that just merges parents could increase the running time of the query.
+ * Let cost(A) be the time it takes to evaluate A.
+ * Let (A, B) be the node that consists of A and B where the children of (A, B) are children(A) \cup children(B).
+ * Evaluation of (A, B) may (and will) occur in parallel, thus the cost((A, B)) = max(cost(A), cost(B)).
+ * Observe the following rewrite and costs:
+ *
+ * cost(B) = 5
+ * cost(C) = 5
+ * cost(E) = 10
+ * cost(D) = 10
+ *
+ *            A 
+ *           / \
+ *          B   E
+ *          |   | \
+ *          C   D  D
+ *         / \
+ *        D  D
+ *
+ *            A 
+ *            | 
+ *         (B, E)  
+ *           |   
+ *      (C, Defer) 
+ *          |
+ *         [D] 
+ *
+ * Calculating the maximum time of execution for the first query max(B(5) + C(5) + D(10), E(10) + D(10)) = 20
+ * And the second = max(B(5), E(10)) + C(5) + D(10) = 25
+ * Clearly the running time has been traded for more batching.
+ * There are a couple of choices to be made here which essentially boil down to how one chooses to calculate cost.
+ * How does one measure 'batchability', the reduction in cost of batching? 
+ * Maybe for a contraction [V] where the navie execution cost is |[V]| * cost(V), 
+ * the 'batchability' could be measured by cost(V) - (|[V]| - 1) * b(V) where b(V) is the cost savings of batching?.
  */
 object Optimizer {
   // def optimize[F[_]]()
