17005860e786a51bd8924ccf34050987dc97233a
Valdemar Grange <randomvald0069@gmail.com>
2022-08-13 22:23:21 +0200

    stable batch impl

--- src/main/scala/gql/resolver/BatchResolver.scala
+++ src/main/scala/gql/resolver/BatchResolver.scala
@@ -6,14 +6,14 @@ import cats.effect._
 
 final case class BatchResolver[F[_], I, K, A, T](
     // Pick batch implementation based on input
-    batcher: I => BatcherReference[K, T],
+    batcher: BatcherReference[K, T],
     partition: I => F[Batch[F, K, A, T]]
 ) extends LeafResolver[F, I, A] {
   def flatMapF[B](f: A => F[B])(implicit F: FlatMap[F]) =
     BatchResolver(batcher, partition.andThen(_.map(_.flatMapF(f))))
 
   def contraMap[B](g: B => I): BatchResolver[F, B, K, A, T] =
-    BatchResolver(i => batcher(g(i)), g.andThen(partition))
+    BatchResolver(batcher, g.andThen(partition))
 
   def mapK[G[_]: MonadCancelThrow](fk: F ~> G): BatchResolver[G, I, K, A, T] =
     BatchResolver(batcher, partition.andThen(fa => fk(fa).map(bp => bp.copy(post = bp.post.andThen(fk.apply)))))
