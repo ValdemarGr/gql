b7f2f372b0262c92554ec202816f28ea48b4d9ca
Valdemar Grange <randomvald0069@gmail.com>
2022-08-14 13:25:25 +0200

    coool

--- src/main/scala/gql/resolver/SignalResolver.scala
+++ src/main/scala/gql/resolver/SignalResolver.scala
@@ -4,6 +4,10 @@ import cats.effect._
 import cats.implicits._
 import cats._
 
+sealed trait ConstrainedPipe[A, B] {
+  def pipe[F[_]: Async]: fs2.Pipe[F, A, B]
+}
+
 final case class SignalResolver[F[_]: MonadCancelThrow, I, A, B](
     // No elem
     head: LeafResolver[F, I, A],
@@ -12,14 +16,20 @@ final case class SignalResolver[F[_]: MonadCancelThrow, I, A, B](
     tail: I => F[SignalResolver.DataStreamTail[I, A]],
     // Post-processing of both head and tail, allows a map function on the structure
     postHead: (I, A) => F[B],
-    postTail: (I, A) => fs2.Stream[F, B]
+    // This cannot be a pipe, since we, the definer of this resolver
+    // don't control F, the toplevel interpreter of the resolver has some F bound
+    // To continue allowing complex control of the stream we can instead summon the typeclass which allows
+    // any combinator in fs2; Async. Attempting to construct mapK and contraMap with fs2.Pipe is impossible
+    // and one must instead constrain the implementation to a bidirectional mapping F ~> G and G ~> F for mapK
+    // and C => I and I => C for contraMap.
+    postTail: (I, A) => ConstrainedPipe[A, B]
 ) extends Resolver[F, I, B] {
   def mapK[G[_]: MonadCancelThrow](fk: F ~> G): SignalResolver[G, I, A, B] =
     SignalResolver(
       head.mapK(fk),
       i => fk(tail(i)),
       (i, a) => fk(postHead(i, a)),
-      (i, a) => postTail(i, a).translate(fk)
+      postTail
     )
 
   def contramap[C](g: C => I): SignalResolver[F, C, A, B] =
@@ -27,14 +37,23 @@ final case class SignalResolver[F[_]: MonadCancelThrow, I, A, B](
       head.contramap(g),
       i => tail(g(i)).map(dst => dst.copy(ref = StreamReference(dst.ref.id))),
       (i, a) => postHead(g(i), a),
-      (i, a) => postTail(g(i), a)
+      (i, a) =>
+        new ConstrainedPipe[A, B] {
+          def pipe[F[_]: Async]: fs2.Pipe[F, A, B] = postTail(g(i), a).pipe[F]
+        }
     )
 
-  def flatMapF[C](f: B => F[C])(implicit F: FlatMap[F]): SignalResolver[F, I, A, C] =
-    SignalResolver(head, tail, (i, a) => postHead(i, a).flatMap(f), (i, a) => postTail(i, a).evalMap(f))
-
-  def through(pipe: fs2.Pipe[F, (I, A, B), B]): SignalResolver[F, I, A, B] =
-    copy(postTail = (i, a) => postTail(i, a).map(b => (i, a, b)).through(pipe))
+  def through[C](headOp: B => F[C], tailOp: ConstrainedPipe[(I, A, B), C])(implicit F: FlatMap[F]): SignalResolver[F, I, A, C] =
+    SignalResolver(
+      head,
+      tail,
+      postHead = (i, a) => postHead(i, a).flatMap(headOp),
+      postTail = (i, a) =>
+        new ConstrainedPipe[A, C] {
+          def pipe[F[_]: Async]: fs2.Pipe[F, A, C] =
+            _.through(postTail(i, a).pipe[F]).map(b => (i, a, b)).through(tailOp.pipe[F])
+        }
+    )
 }
 
 object SignalResolver {
