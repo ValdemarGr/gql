b438c241c5d728517816c8aeecb0a3ea207ae03d
Valdemar Grange <randomvald0069@gmail.com>
2022-09-02 23:50:03 +0200

    work on executor

--- src/main/scala/gql/Execute.scala
+++ src/main/scala/gql/Execute.scala
@@ -9,7 +9,7 @@ import gql.interpreter.Interpreter
 
 object Execute {
   sealed trait ExecutorOutcome[F[_], Q, M, S]
-  object ExecutionOutcome {
+  object ExecutorOutcome {
     final case class ValidationError[F[_], Q, M, S](msg: String) extends ExecutorOutcome[F, Q, M, S]
     final case class Query[F[_], Q, M, S](run: Q => F[JsonObject]) extends ExecutorOutcome[F, Q, M, S]
     final case class Mutation[F[_], Q, M, S](run: M => F[JsonObject]) extends ExecutorOutcome[F, Q, M, S]
@@ -21,17 +21,17 @@ object Execute {
       schema: Schema[F, Q],
       variables: Map[String, Json],
       schemaState: SchemaState[F]
-  )(implicit F: Async[F]) = {
+  )(implicit F: Async[F]): ExecutorOutcome[F, Q, M, S] = {
     PreparedQuery.prepare2(query, schema, variables) match {
-      case Left(err) => F.pure(Left(err))
+      case Left(err) => ExecutorOutcome.ValidationError(err)
       case Right(x) =>
         x match {
           case PreparedQuery.OperationType.Query(rootFields) =>
-            ExecutionOutcome.Query[F, Q, M, S](Interpreter.runSync(_, rootFields, schemaState))
+            ExecutorOutcome.Query[F, Q, M, S](Interpreter.runSync(_, rootFields, schemaState))
           case PreparedQuery.OperationType.Mutation(rootFields) =>
-            ExecutionOutcome.Mutation[F, Q, M, S](Interpreter.runSync(_, rootFields, schemaState))
+            ExecutorOutcome.Mutation[F, Q, M, S](Interpreter.runSync(_, rootFields, schemaState))
           case PreparedQuery.OperationType.Subscription(dataStream, root) =>
-            ExecutionOutcome.Stream[F, Q, M, S] { s =>
+            ExecutorOutcome.Stream[F, Q, M, S] { s =>
               dataStream(s).switchMap(Interpreter.runStreamed[F](_, NonEmptyList.one(root), schemaState))
             }
         }
