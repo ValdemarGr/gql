0d52c66bde6aebcc10d394b92357074ec27ef194
Valdemar Grange <randomvald0069@gmail.com>
2022-09-18 20:18:15 +0200

    dsl work

--- modules/core/src/main/scala/gql/dsl.scala
+++ modules/core/src/main/scala/gql/dsl.scala
@@ -1,5 +1,12 @@
 package gql
 
-object dsll {
+import gql.ast._
+import cats.data._
 
+object dsl {
+  def tpe[F[_], A](
+      name: String,
+      hd: (String, Field[F, A, _, _]),
+      tl: (String, Field[F, A, _, _])*
+  ) = Type[F, A](name, NonEmptyList(hd, tl.toList))
 }
