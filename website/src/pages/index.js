4ef43db5664e2312ed2305b5d52e5faadbf24863
Valdemar Grange <randomvald0069@gmail.com>
2022-09-21 19:23:05 +0200

    fix broken link

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/example">
+            to="/docs/overview/example">
             Example
     {/*Docusaurus Tutorial - 5min ⏱️*/}
           </Link>
