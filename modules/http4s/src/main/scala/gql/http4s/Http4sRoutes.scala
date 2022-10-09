51747b16770c56e242fa811197a234594d7ee41b
Valdemar Grange <randomvald0069@gmail.com>
2022-10-10 00:43:38 +0200

    wo

--- modules/http4s/src/main/scala/gql/http4s/Http4sRoutes.scala
+++ modules/http4s/src/main/scala/gql/http4s/Http4sRoutes.scala
@@ -13,10 +13,13 @@ import fs2.{Stream, Pure}
 import org.http4s.headers.Authorization
 import gql.interpreter.Interpreter
 import gql._
+import org.http4s.server.websocket.WebSocketBuilder
 
 object Http4sRoutes {
-  def apply[F[_], Q, M, S](
-      schema: Schema[F, Q, M, S],
+  implicit lazy val cd = io.circe.generic.semiauto.deriveDecoder[CompilerParameters]
+  implicit def ed[F[_]: Concurrent] = org.http4s.circe.jsonOf[F, CompilerParameters]
+
+  def sync[F[_]](
       compiler: Http4sCompiler[F],
       path: String = "graphql"
   )(implicit F: Concurrent[F]) = {
@@ -24,8 +27,6 @@ object Http4sRoutes {
     import d._
     import io.circe.syntax._
     import org.http4s.circe._
-    implicit lazy val cd = io.circe.generic.semiauto.deriveDecoder[CompilerParameters]
-    implicit lazy val ed = org.http4s.circe.jsonOf[F, CompilerParameters]
 
     HttpRoutes.of[F] { case r @ POST -> Root / `path` =>
       r.as[CompilerParameters].flatMap { params =>
@@ -41,6 +42,21 @@ object Http4sRoutes {
             fa.flatMap(qr => Ok(qr.asGraphQL.asJson))
         }
       }
+    }
+  }
+
+  def ws[F[_]](
+      compiler: Http4sCompiler[F],
+      path: String = "ws",
+      wsb: WebSocketBuilder[F]
+  )(implicit F: Concurrent[F]) = {
+    val d = new Http4sDsl[F] {}
+    import d._
+    import io.circe.syntax._
+    import org.http4s.circe._
+
+    HttpRoutes.of[F] {
+      case r @ GET -> Root / `path` => ???
     }
   }
 }
