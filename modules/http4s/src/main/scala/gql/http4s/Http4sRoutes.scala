cc1349cfa70fd343b4aff394083b4e4e2256b7e7
Valdemar Grange <randomvald0069@gmail.com>
2022-10-09 21:37:23 +0200

    wok

--- modules/http4s/src/main/scala/gql/http4s/Http4sRoutes.scala
+++ modules/http4s/src/main/scala/gql/http4s/Http4sRoutes.scala
@@ -15,44 +15,7 @@ import gql.interpreter.Interpreter
 import gql._
 
 object Http4sRoutes {
-  final case class Http4sCompilerParametes(
-      compilerParameters: CompilerParameters,
-      headers: Headers
-  )
-
-  trait Http4sCompiler[F[_]] {
-    def compile(params: Http4sCompilerParametes): F[Either[Response[F], Application[F]]]
-  }
-
-  object Http4sCompiler {
-    def apply[F[_]](compiler: Http4sCompilerParametes => F[Either[Response[F], Compiler.Outcome[F]]])(implicit
-        F: Async[F]
-    ): Http4sCompiler[F] =
-      new Http4sCompiler[F] {
-        def compile(params: Http4sCompilerParametes): F[Either[Response[F], Application[F]]] = {
-          val dsl = new org.http4s.dsl.Http4sDsl[F] {}
-          import dsl._
-          import org.http4s.circe._
-          import io.circe.syntax._
-
-          compiler(params).flatMap(_.flatTraverse {
-            case Left(compErr) =>
-              compErr match {
-                case CompilationError.Parse(pe)       => BadRequest(pe.asGraphQL.asJson).map(_.asLeft)
-                case CompilationError.Preparation(pe) => BadRequest(pe.asGraphQL.asJson).map(_.asLeft)
-              }
-            case Right(application) => F.pure(Right(application))
-          })
-        }
-      }
-
-    def fromCompiler[F[_]](compiler: Http4sCompilerParametes => F[Either[Response[F], Compiler[F]]])(implicit
-        F: Async[F]
-    ): Http4sCompiler[F] =
-      apply[F](params => compiler(params).flatMap(_.traverse(_.compile(params.compilerParameters))))
-  }
-
-  def simple[F[_], Q, M, S](
+  def apply[F[_], Q, M, S](
       schema: Schema[F, Q, M, S],
       compiler: Http4sCompiler[F],
       path: String = "graphql"
