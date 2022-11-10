e74a394e1ffa5536a560e31c3bef68757e5819fa
Valdemar Grange <randomvald0069@gmail.com>
2022-11-10 13:44:56 +0100

    hm

--- modules/goi/src/main/scala/gql/goi/Node.scala
+++ modules/goi/src/main/scala/gql/goi/Node.scala
@@ -1,10 +1,13 @@
 package gql.goi
 
-// import cats.implicits._
+import gql.ast._
+import gql.dsl._
+import cats.effect._
+import java.util.Base64
+import java.nio.charset.StandardCharsets
+import cats.implicits._
 
-trait Node {
-  def id: String
-}
+trait Node
 
 final case class NodeId(id: String)
 
@@ -13,11 +16,19 @@ trait NodeInstance[F[_], A] {
 }
 
 object Node {
-  def fromTypename[F[_], A](typename: String, get: String => F[Option[A]], id: A => String) = {
-    def get0(id: String) = id.split(':').toList match {
-      case x :: xs if x == typename => Some(get(xs.mkString(":")))
-      case _                        => None
-    }
-    def id0(a: A) = s"$typename:${id(a)}"
+  def fromTypename[F[_], A](typename: String, get: String => F[Option[A]], id: A => String)(implicit F: Sync[F]) = {
+    def get0(id: String) =
+      F.delay(new String(Base64.getDecoder.decode(id), StandardCharsets.UTF_8)).map(_.split(':').toList).map {
+        case x :: xs if x == typename => Some(get(xs.mkString(":")))
+        case _                        => None
+      }
+    def id0(a: A) =
+      F.delay(new String(Base64.getEncoder.encode(s"$typename:${id(a)}".getBytes()), StandardCharsets.UTF_8))
+    (get0 _, id0 _)
+  }
+
+  def fromType[F[_]: Sync, A <: Node](get: String => F[Option[A]], id: A => String)(tpe: Type[F, A]) = {
+    val (get0, id0) = fromTypename(tpe.name, get, id)
+    // tpe.imple
   }
 }
