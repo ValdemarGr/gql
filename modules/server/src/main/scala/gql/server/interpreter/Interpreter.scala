a44e70ce2d17ed8fa28c1d88bd7135398de6ad49
ValdemarGr <randomvald0069@gmail.com>
2023-05-30 19:07:18 +0200

    fix: left vs inner join-ish error

--- modules/server/src/main/scala/gql/server/interpreter/Interpreter.scala
+++ modules/server/src/main/scala/gql/server/interpreter/Interpreter.scala
@@ -555,6 +555,7 @@ class InterpreterImpl[F[_]](
   def startNext[I](s: Prepared[F, I], in: Chain[EvalNode[F, I]]): W[Chain[Json]] = W.defer {
     s match {
       case PreparedLeaf(_, enc) => W.pure(in.map(x => enc(x.value)))
+      case Selection(Nil)       => W.pure(in.as(Json.obj()))
       case Selection(fields)    => runFields(fields, in).map(_.map(JsonObject.fromMap(_).asJson))
       case s: PreparedList[F, a, ?, b] =>
         val of = s.of
