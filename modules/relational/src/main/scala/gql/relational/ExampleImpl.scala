9d4c6705b5fd32e986376aa0085932026c4375af
Valdemar Grange <randomvald0069@gmail.com>
2023-08-15 00:58:52 +0200

    unused

--- modules/relational/src/main/scala/gql/relational/ExampleImpl.scala
+++ modules/relational/src/main/scala/gql/relational/ExampleImpl.scala
@@ -5,28 +5,11 @@ import skunk.implicits._
 import gql.ast._
 import gql.dsl._
 import cats.implicits._
-import fs2.Pure
 import skunk._
 import cats._
-import gql.resolver.Resolver
-import cats.data._
-import scala.reflect.ClassTag
 import java.util.UUID
 import gql.Arg
 import gql.EmptyableArg
-import gql.resolver.FieldMeta
-import cats.mtl.Stateful
-import cats.mtl.Tell
-import gql.{preparation => prep}
-import cats.mtl.Raise
-import gql.Schema
-import gql.SchemaShape
-import gql.Application
-import natchez.TraceValue
-import natchez.Kernel
-import natchez.Span
-import java.net.URI
-import cats.arrow.FunctionK
 import skunk.codec.all._
 
 object SkunkSchema extends QueryAlgebra with QueryDsl {
