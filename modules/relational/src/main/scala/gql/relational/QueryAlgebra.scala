d2a0bd359ca70464fb752e865cc24a874d893a45
Valdemar Grange <randomvald0069@gmail.com>
2023-08-15 00:58:52 +0200

    unused

--- modules/relational/src/main/scala/gql/relational/QueryAlgebra.scala
+++ modules/relational/src/main/scala/gql/relational/QueryAlgebra.scala
@@ -1,33 +1,14 @@
 package gql.relational
 
-import cats.effect._
-import skunk.implicits._
-import gql.ast._
-import gql.dsl._
 import cats.implicits._
-import fs2.Pure
-import skunk.codec._
 import skunk._
 import cats._
 import gql.resolver.Resolver
 import cats.data._
-import scala.reflect.ClassTag
-import java.util.UUID
-import gql.Arg
-import gql.EmptyableArg
-import gql.resolver.FieldMeta
-import cats.mtl.Stateful
-import cats.mtl.Tell
-import gql.{preparation => prep}
-import cats.mtl.Raise
-import gql.Schema
-import gql.SchemaShape
-import gql.Application
-import natchez.TraceValue
-import natchez.Kernel
-import natchez.Span
-import java.net.URI
+import cats.mtl._
 import cats.arrow.FunctionK
+import gql.ast._
+import gql.EmptyableArg
 
 trait QueryAlgebra {
   import QueryAlgebra.{QueryState => _, QueryStateImpl => _, _}
