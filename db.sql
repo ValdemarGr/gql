25400e9d9901988fc059129a01bfb9518eff0763
Valdemar Grange <randomvald0069@gmail.com>
2023-09-02 18:02:59 +0200

    success

--- db.sql
+++ db.sql
@@ -12,8 +12,18 @@ create table entity (
     height int not null
 );
 
+create table pet (
+    id uuid not null,
+    name text not null
+);
+
 create table contract_entity (
     contract_id uuid not null,
+    entity_id uuid not null
+);
+
+create table pet_entity (
+    pet_id uuid not null,
     entity_id uuid not null
 );
 
@@ -39,5 +49,28 @@ insert into contract_entity (contract_id, entity_id) values (
     'c4958a82-7ba8-498e-8220-a5b10c047229'
 ), (
     '1ff0ca77-c13f-4af8-9166-72373f309247',
+    'e787e1b5-4edb-4519-8bd7-8018946c1e2a'
+);
+
+insert into pet (id, name) values (
+    '1ff0ca77-c13f-4af8-9166-72373f309247',
+    'Dog'
+), (
+    '1ff0ca77-c13f-4af8-9166-72373f309248',
+    'Cat'
+);
+
+-- Both john and jane own Dog and Cat
+insert into pet_entity (pet_id, entity_id) values (
+    '1ff0ca77-c13f-4af8-9166-72373f309247',
+    'c4958a82-7ba8-498e-8220-a5b10c047229'
+), (
+    '1ff0ca77-c13f-4af8-9166-72373f309248',
+    'c4958a82-7ba8-498e-8220-a5b10c047229'
+), (
+    '1ff0ca77-c13f-4af8-9166-72373f309247',
+    'e787e1b5-4edb-4519-8bd7-8018946c1e2a'
+), (
+    '1ff0ca77-c13f-4af8-9166-72373f309248',
     'e787e1b5-4edb-4519-8bd7-8018946c1e2a'
 );
