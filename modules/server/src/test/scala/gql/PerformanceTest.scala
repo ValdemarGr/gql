2a3620580f8b19cf7d2bafd7977f2da4262f721a
Valdemar Grange <randomvald0069@gmail.com>
2024-01-12 21:40:15 +0100

    rm timeout

--- modules/server/src/test/scala/gql/PerformanceTest.scala
+++ modules/server/src/test/scala/gql/PerformanceTest.scala
@@ -26,8 +26,6 @@ import scala.concurrent.duration._
 import fs2.concurrent.SignallingRef
 
 class PerformanceTest extends CatsEffectSuite {
-  override def munitIOTimeout: Duration = 1.minutes
-
   def parListenSignal[A]: fs2.Pipe[IO, fs2.Stream[IO, A], A] =
     streams =>
       Stream.eval(SignallingRef.of[IO, Option[A]](None)).flatMap { sig =>
